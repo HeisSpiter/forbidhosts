f1607136bf867288871618439727f0b8b14dc19b
Pierre Schweitzer <pierre@reactos.org>
2014-08-27 12:12:34 +0200

    Rename project to ForbidHosts to reflect last changes

--- ForbidHosts.cpp
+++ ForbidHosts.cpp
@@ -16,7 +16,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "ForbidHostsv6.h"
+#include "ForbidHosts.h"
 
 #include <arpa/inet.h>
 #include <sys/socket.h>
